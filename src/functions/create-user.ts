a26e76114e2b81fc588933d8fc354e5ff08c6f7d
Dimas Paiva <Dimas.Paiva@ctw.bmwgroup.com>
2022-10-25 07:44:35 +0100

    feat: validate data on user creation

--- src/functions/create-user.ts
+++ src/functions/create-user.ts
@@ -1,7 +1,18 @@
 import { CreateUser } from "../models/User";
 
 export const createUser: CreateUser = async (user) => {
-  const { password: _, ...newUser } = user;
+  const { password, ...newUser } = user;
+
+  if (!newUser.name) {
+    throw new Error("Failed to create user - Missing property name");
+  }
+  if (!newUser.email) {
+    throw new Error("Failed to create user - Missing property email");
+  }
+  if (!password) {
+    throw new Error("Failed to create user - Missing property password");
+  }
+
   return {
     id: Math.floor(Math.random() * 10000).toString(),
     ...newUser,
--- src/test/functions/create-user.test.ts
+++ src/test/functions/create-user.test.ts
@@ -16,4 +16,43 @@ describe("Create User", () => {
     expect(user).toHaveProperty("id");
     expect(user).not.toHaveProperty("password");
   });
+
+  it("should fail to create an user without name", async () => {
+    const dummyUserData = {
+      name: "",
+      email: "jhon.doe@test.ts",
+      birthday: new Date("1999-01-01"),
+      password: "123456",
+    };
+
+    await expect(createUser(dummyUserData)).rejects.toThrowError(
+      "Failed to create user - Missing property name"
+    );
+  });
+
+  it("should fail to create an user without name", async () => {
+    const dummyUserData = {
+      name: "Jhon Doe",
+      email: "",
+      birthday: new Date("1999-01-01"),
+      password: "123456",
+    };
+
+    await expect(createUser(dummyUserData)).rejects.toThrowError(
+      "Failed to create user - Missing property email"
+    );
+  });
+
+  it("should fail to create an user without name", async () => {
+    const dummyUserData = {
+      name: "Jhon Doe",
+      email: "jhon.doe@test.ts",
+      birthday: new Date("1999-01-01"),
+      password: "",
+    };
+
+    await expect(createUser(dummyUserData)).rejects.toThrowError(
+      "Failed to create user - Missing property password"
+    );
+  });
 });
